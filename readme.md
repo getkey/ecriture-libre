edeaaed45e389dd0eacb6cf340e4ee93fad196bc
Julien Mourer <getkey@getkey.eu>
2022-01-21 16:47:20 +0100

    Make file configurable

---
+++ readme.md
@@ -0,0 +1,4 @@
+# Écriture libre
+
+Ce programme crée un environnement où chaque mot, une fois écrit, ne peut pas être supprimé.
+Le but est de forcer l'[écriture libre](https://en.wikipedia.org/wiki/Free_writing).
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use clap::Parser;
 use nix::sys::termios;
 use std::fs::OpenOptions;
 use std::io;
@@ -5,7 +6,13 @@ use std::io::{Read, Write};
 #[cfg(unix)]
 use std::os::unix::io::AsRawFd;
 
+#[derive(Parser, Debug)]
+struct Args {
+	file: String,
+}
+
 fn main() -> io::Result<()> {
+	let args = Args::parse();
 	let stdin = io::stdin();
 
 	let old_termios = termios::tcgetattr(stdin.as_raw_fd())?;
@@ -25,31 +32,31 @@ fn main() -> io::Result<()> {
 		.write(true)
 		.create(true)
 		.append(true)
-		.open("/tmp/ecriture_libre.ecl")?;
+		.open(args.file)?;
 
-    let mut deletable_chars = 0;
+	let mut deletable_chars = 0;
 
 	for byte in stdin.bytes() {
 		match byte? as char {
 			'\x03' | '\x04' => break,
 			' ' => {
-                buffer.push(' ');
+				buffer.push(' ');
 				f.write(buffer.as_bytes())?;
 				buffer.clear();
 				print!(" ");
-                deletable_chars = 0;
+				deletable_chars = 0;
 			}
 			'\x7f' => {
-                if deletable_chars > 0 {
-                    print!("\x1B[1D \x1B[1D");
-                    buffer.pop();
-                    deletable_chars -= 1;
-                }
+				if deletable_chars > 0 {
+					print!("\x1B[1D \x1B[1D");
+					buffer.pop();
+					deletable_chars -= 1;
+				}
 			}
 			b => {
 				buffer.push(b);
 				print!("{}", b);
-                    deletable_chars += 1;
+				deletable_chars += 1;
 			}
 		}
 		io::stdout().flush()?;
